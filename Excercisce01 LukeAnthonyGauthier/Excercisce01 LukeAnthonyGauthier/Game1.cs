51bc7db1a55c0a4465208941aa9dd8bb68acf666
LukeAnthonyGauthier <gauthier.luke-anthony@carrefour.cegepvicto.ca>
2016-11-16 17:03:39 -0500

    Version avec ennemie normal et music de fond
    il amnque l ajout de song de  mort les pouple fond des bruit  mais il
    manque toujours les type de munition

--- Excercisce01 LukeAnthonyGauthier/Excercisce01 LukeAnthonyGauthier/Game1.cs
+++ Excercisce01 LukeAnthonyGauthier/Excercisce01 LukeAnthonyGauthier/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
 
 namespace Excercisce01_LukeAnthonyGauthier
 {
@@ -11,6 +14,20 @@ namespace Excercisce01_LukeAnthonyGauthier
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Rectangle fenetre;
+        GameObject heros;
+        GameObject ennemie;
+        GameObject[] tabBullet = new GameObject[40];
+        Random Rnd = new Random();
+        Texture2D background;
+        SoundEffect bulletSong ;
+        SoundEffectInstance bulletSongs;
+        SoundEffect gameover;
+        SoundEffectInstance gameovers;
+
+        bool amorceur = false;
+        int compteur = 0;
+
 
         public Game1()
         {
@@ -27,7 +44,9 @@ namespace Excercisce01_LukeAnthonyGauthier
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
+            this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
+            this.graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -40,7 +59,55 @@ namespace Excercisce01_LukeAnthonyGauthier
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            //TODO: use this.Content to load your game content here
+            fenetre = graphics.GraphicsDevice.Viewport.Bounds;
+            fenetre.Width = graphics.GraphicsDevice.DisplayMode.Width;
+            fenetre.Height = graphics.GraphicsDevice.DisplayMode.Height;
+
+            background= Content.Load<Texture2D>("Background.png");
+
+            bulletSong = Content.Load<SoundEffect>("Sounds\\BulletSong");
+            bulletSongs = bulletSong.CreateInstance();
+
+            Song gameover = Content.Load<Song>("Sounds\\Gameover");
+            gameovers = bulletSong.CreateInstance();
+
+            Song song = Content.Load<Song>("Sounds\\maintrac");
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(song);
+            
+            
+
+
+            heros = new GameObject();
+            heros.estVivant = true;
+            heros.vitesse = 5;
+            heros.sprite = Content.Load<Texture2D>("01.png");
+            heros.position = heros.sprite.Bounds;
+            heros.position.X = 50;
+            heros.position.Y = 50;
+
+            ennemie = new GameObject();
+            ennemie.estVivant = true;
+            ennemie.vitesse = -4;
+            ennemie.sprite = Content.Load<Texture2D>("Boss.png");
+            ennemie.position = ennemie.sprite.Bounds;
+            ennemie.position.X = 1600;
+            ennemie.position.Y = 450;
+
+
+            for (int i = 0; i < 40; i++)
+            {
+                tabBullet[i] = new GameObject();
+                tabBullet[i].estVivant = false;
+                tabBullet[i].sprite = Content.Load<Texture2D>("Projectille.png");
+                tabBullet[i].position = tabBullet[i].sprite.Bounds;
+                tabBullet[i].position.X = ennemie.position.X;
+                tabBullet[i].position.Y = ennemie.position.Y; 
+                       
+            }
+
+
         }
 
         /// <summary>
@@ -64,8 +131,149 @@ namespace Excercisce01_LukeAnthonyGauthier
 
             // TODO: Add your update logic here
 
+            //Hero mouvemant 
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                heros.position.X += heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                heros.position.X -= heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                heros.position.Y -= heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                heros.position.Y += heros.vitesse;
+            }
+
+            UpdateHeros();
+            UpdateEnnemi();
+            UpdateCollision();
+            Updatebullet(gameTime);
+
             base.Update(gameTime);
         }
+
+        protected void UpdateHeros()
+        {
+            if (heros.position.X < fenetre.Left)
+            {
+                heros.position.X = fenetre.Right - heros.sprite.Width;
+            }
+            else if (heros.position.X + heros.sprite.Width > fenetre.Right)
+            {
+                heros.position.X = fenetre.Left;
+            }
+            else if (heros.position.Y + heros.sprite.Height > fenetre.Bottom)
+            {
+                heros.position.Y = fenetre.Top;
+            }
+            else if (heros.position.Y < fenetre.Top)
+            {
+                heros.position.Y = fenetre.Bottom - heros.sprite.Height;
+            }
+            for (int i = 0; i < 40; i++)
+            {
+                if (heros.position.Intersects(tabBullet[i].sprite.Bounds))
+                {
+                    heros.estVivant = false;
+                }
+            }
+
+        }
+        protected void UpdateEnnemi()
+        {
+            ennemie.position.Y += ennemie.vitesse;
+
+            if (ennemie.position.Y + ennemie.sprite.Height > fenetre.Bottom)
+            {
+                ennemie.vitesse = -4;
+            }
+            if (ennemie.position.Y < fenetre.Top)
+            {
+                ennemie.vitesse = +4;
+            }
+        }
+        protected void Updatebullet(GameTime gameTime)
+        {
+            if (ennemie.estVivant == true)
+            {
+                for (int i = 0; i < 40; i++)
+                {
+                    tabBullet[i].position.X += (tabBullet[i].vitesse);
+
+                    if (tabBullet[i].position.X < fenetre.Left && compteur!=3)
+                    {                    
+                        tabBullet[i].estVivant = false;
+                        compteur++;
+                        if (compteur == 3)
+                        {
+                            tabBullet[i].position.X = tabBullet[i].vitesse*-1;
+                            //tabBullet[i].position.Height *= 2;
+                            //tabBullet[i].position.Width *= 2;
+                            compteur = 0;
+                        }
+                    }                    
+                }
+                for (int i = 0; i < 40; i++)
+                {
+
+                    if ((Rnd.Next(1, 1000) == 1) && (tabBullet[i].estVivant == false))
+                    {
+                        tabBullet[i].estVivant = true;
+                        tabBullet[i].position.X = ennemie.position.X;
+                        tabBullet[i].position.Y = ennemie.position.Y;
+                        tabBullet[i].vitesse = Rnd.Next(-6, -2);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 40; i++)
+                {
+                    tabBullet[i].estVivant = false;
+                }
+            }
+        }
+        protected void UpdateCollision()
+        { 
+            
+            for (int i = 0; i < 40; i++)
+            {
+                if (tabBullet[i].estVivant == true && heros.estVivant == true)
+                {
+                    if (heros.position.Intersects(tabBullet[i].position))
+                    {
+                        heros.estVivant = false;
+                        amorceur = true;
+                        if (amorceur== true)
+                        {
+                            bulletSong.Play();
+                            amorceur = false;
+                        }                          
+                    }
+                }
+            }
+            if (heros.estVivant == true)
+            {
+                if (ennemie.position.Intersects(heros.position))
+                {
+                    ennemie.estVivant = false;
+                }
+            }
+        }
+        protected void UpdateMusic()
+        {
+            if (heros.estVivant == false)
+            {
+                gameovers.Play();
+            }
+        }
+
+
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -75,7 +283,32 @@ namespace Excercisce01_LukeAnthonyGauthier
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            //TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background,fenetre, Color.White);
+
+            
+
+            if (heros.estVivant == true)
+            {
+                spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            }
+            if (ennemie.estVivant == true)
+            {
+                spriteBatch.Draw(ennemie.sprite, ennemie.position, Color.White);
+            }
+            for (int i = 0; i < 40; i++)
+            {
+                if (tabBullet[i].estVivant == true)
+                {                   
+                    spriteBatch.Draw(tabBullet[i].sprite, tabBullet[i].position, Color.White);
+                }
+            }
+
+            spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
---
+++ Excercisce01 LukeAnthonyGauthier/Excercisce01 LukeAnthonyGauthier/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Excercisce01_LukeAnthonyGauthier
+{
+    class GameObject
+    {
+        public Rectangle position;
+        public int vitesse;
+        public Texture2D sprite;
+        public bool estVivant;
+    }
+}
